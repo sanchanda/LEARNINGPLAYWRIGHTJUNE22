dbf7524776b4823f9e9036e7a9a758098b31001d
sanchanda <santoshkumar.chanda1@gmail.com>
2026-06-23 14:53:34 +0530

    first commit

---
+++ chapter_01_Basics/01_Basics.js
@@ -0,0 +1,6 @@
+console.log("Hello Gure Saibaba! Welcome to JavaScript Basics.");
+/*console.log(process.platform);
+console.log(process.version);
+console.log(process.arch);
+console.log(process.cwd()); */
+console.log(process.env);
