cd9526fcacad8f433f76e86621e4319a85a12aea
sanchanda <santoshkumar.chanda1@gmail.com>
2026-06-23 21:51:15 +0530

    Add chapter 02 and 03 JS files

---
+++ chapter_03_Identifier_Literals/08_Comments.js
@@ -0,0 +1,19 @@
+/* Multiline comments
+* Author : Santosh
+* Date : 2024-06-22
+*/
+
+// al+shift + down arrow to copy the line above
+// al+shift + down arrow to copy the line above
+// Single line comment
+// Single line comment
+// Single line comment
+// Single line comment
+// Single line comment
+// Single line comment
+// Single line comment
+// Single line comment
+// Single line comment
+// Single line comment
+// Single line comment
+
